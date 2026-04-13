ed88a922e06b7dcecd464dd123968860c4a63966
sisiliakewae <sisiliakewae@gmail.com>
2026-04-13 12:57:33 +0700

    fixing ui

--- CustomerMenuPage.xaml.cs
+++ CustomerMenuPage.xaml.cs
@@ -6,4 +6,22 @@ public partial class CustomerMenuPage : ContentPage
     {
         InitializeComponent();
     }
+
+    private async void OnCategoryClicked(object sender, EventArgs e)
+    {
+        if (sender is Button btn)
+        {
+            await DisplayAlert("Kategori", $"Kamu memilih kategori: {btn.Text}", "OK");
+        }
+    }
+
+    private async void OnAddToCartClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Keranjang", "Menu berhasil ditambahkan ke keranjang!", "OK");
+    }
+
+    private async void OnCartIconTapped(object sender, TappedEventArgs e)
+    {
+        await DisplayAlert("Keranjang Pesanan", "Membuka halaman keranjang pesananmu...", "OK");
+    }
 }
